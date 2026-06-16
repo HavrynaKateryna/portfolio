1634bef5f43585cc952743d4eec953a3bf385533
Kateryna Havryna <khavryna@gmail.com>
2026-06-16 02:11:24 +0200

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import "./globals.css";
-import Header from "../components/Header";
-
 export const metadata = {
+  metadataBase: new URL("https://www.katerynahavryna.com"),
+
   title: {
-    default: " Kateryna Havryna",
+    default: "Kateryna Havryna",
     template: "%s | Kateryna Havryna",
   },
+
   description:
     "Full-Stack Developer specializing in React, Next.js and TypeScript",
 
@@ -15,22 +15,28 @@ export const metadata = {
     "TypeScript",
     "Frontend Developer",
     "Full Stack Developer",
+    "Portfolio",
   ],
 
   authors: [{ name: "Kateryna Havryna" }],
   creator: "Kateryna Havryna",
 
+  robots: {
+    index: true,
+    follow: true,
+  },
+
   icons: {
-    icon: "/favicon.jpg",
-    shortcut: "/favicon.jpg",
-    apple: "/favicon.jpg",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
   },
 
   openGraph: {
     title: "Kateryna Havryna",
     description:
       "Full-Stack Developer specializing in React, Next.js and TypeScript",
-    url: "https://your-domain.com",
+    url: "https://www.katerynahavryna.com",
     siteName: "Kateryna Portfolio",
     type: "website",
     images: [
@@ -49,35 +55,8 @@ export const metadata = {
       "Full-Stack Developer specializing in React, Next.js and TypeScript",
     images: ["/og.jpg"],
   },
+
+  other: {
+    "google-site-verification": "Nw3d3cF8RZXiyOFS-DhYDNZmR3SD-gkGN_uLc_5WRB8",
+  },
 };
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className="relative min-h-screen bg-black text-white overflow-x-hidden">
-
-        {/* BACKGROUND BLOBS */}
-        <div className="bg">
-          <div className="blob1" />
-          <div className="blob2" />
-        </div>
-
-        {/* NOISE */}
-        <div className="noise" />
-
-        {/* HEADER */}
-        <Header />
-
-        {/* MAIN CONTENT */}
-        <main className="relative z-10 pt-0">
-          {children}
-        </main>
-
-      </body>
-    </html>
-  );
-}
